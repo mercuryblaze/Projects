9e5ee04caffd8045cf25cdec558da9c170ffbd5e
mercuryblaze <77534657+mercuryblaze@users.noreply.github.com>
2021-09-21 14:23:33 +0300

    Add files via upload

---
+++ Number Of Even And Odd Numbers/Number Of Even And Odd Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Number_Of_Even_And_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint evenNumbersCount, oddNumbersCount = evenNumbersCount = 0;
+            Console.WriteLine("Введите число с которого нужно начать отсчёт:");
+            int currentValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число на котором нужно завершить отсчёт:");
+            uint maxValue = uint.Parse(Console.ReadLine());
+            int evenNumbersSum, oddNumbersSum = evenNumbersSum = 0;
+            while (currentValue <= maxValue)
+            {
+                if (currentValue % 2 == 0)
+                {
+                    evenNumbersCount++;
+                    evenNumbersSum += currentValue;
+                }
+                else
+                {
+                    oddNumbersCount++;
+                    oddNumbersSum += currentValue;
+                }
+                currentValue++;
+            }
+            Console.WriteLine("Количество четных чисел в диапазоне = " + evenNumbersCount);
+            Console.WriteLine("Количество нечетных чисел в диапазоне = " + oddNumbersCount);
+            Console.WriteLine("Сумма четных чисел в диапазоне = " + evenNumbersSum);
+            Console.WriteLine("Сумма нечетных чисел в диапазоне = " + oddNumbersSum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Simple Calculator/Simple Calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstNumber, secondNumber;
+                string operation;
+                try
+                {
+                    Console.WriteLine("Введите первое число:");
+                    firstNumber = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число:");
+                    secondNumber = double.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка. Введите только число.");
+                    Console.ReadLine();
+                    continue; // после этого ключевого слова код ниже выполняться не будет, а мы попадём в начало нашего цикла while
+                }
+                Console.WriteLine("Выберите арифметическую операцию: '+', '-', '*', '/'.");
+                operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine("Сумма чисел = " + (firstNumber + secondNumber));
+                        break;
+                    case "-":
+                        Console.WriteLine("Разность чисел = " + (firstNumber - secondNumber));
+                        break;
+                    case "*":
+                        Console.WriteLine("Произведение чисел = " + firstNumber * secondNumber);
+                        break;
+                    case "/":
+                        if (secondNumber == 0) Console.WriteLine("На ноль делить нельзя!");
+                        else Console.WriteLine("Частное чисел = " + firstNumber / secondNumber);
+                        break;
+                    default:
+                        Console.WriteLine("Этот символ не предусмотрен логикой программы");
+                        break;
+                }
+                // Способ №2
+                if (operation == "+") Console.WriteLine("Сумма чисел = " + (firstNumber + secondNumber));
+                else if (operation == "-") Console.WriteLine("Разность чисел = " + (firstNumber - secondNumber));
+                else if (operation == "*") Console.WriteLine("Произведение чисел = " + firstNumber * secondNumber);
+                else if (operation == "/")
+                {
+                    if (secondNumber == 0) Console.WriteLine("На ноль делить нельзя!");
+                    else Console.WriteLine("Частное чисел = " + firstNumber / secondNumber);
+                }
+                else Console.WriteLine("Этот символ не предусмотрен логикой программы");
+                Console.ReadKey();
+            }
+        }
+    }
+}
