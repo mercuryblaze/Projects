a1cf9860156e695c47f433bd08883b6bbc2f71ae
mercuryblaze <77534657+mercuryblaze@users.noreply.github.com>
2021-09-22 22:45:15 +0300

    Add files via upload

---
+++ Console Triangles/Console Triangles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Console_Triangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите высоту треугольника:");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i + 1; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Console Triangles/Version 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Version_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите высоту треугольника:");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.SetCursorPosition(height - i - 1, i + 2);
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("|");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
