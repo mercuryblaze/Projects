8d080a86b90cd4abaf31a62dea987f61c88740d0
mercuryblaze <77534657+mercuryblaze@users.noreply.github.com>
2021-09-24 00:17:03 +0300

    Add files via upload

---
+++ Actions With Arrays №1/Actions With Arrays №1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Actions_With_Arrays__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int[] myArray = new int[int.Parse(Console.ReadLine())];
+
+            Console.WriteLine("\nЗаполните массив целыми числами\n");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nЧисла, содержащиеся в массиве:");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+            Console.WriteLine("\nЧисла, содержащиеся в массиве в обратном порядке:");
+            for (int i = myArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+            int arrayEvenSum = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 == 0) arrayEvenSum += myArray[i];
+            }
+            Console.WriteLine("\nСумма чётных чисел в массиве = " + arrayEvenSum);
+
+            int minInArray = myArray[0];
+            for (int i = 1; i < myArray.Length; i++)
+            {
+                if (myArray[i] < minInArray) minInArray = myArray[i];
+            }
+            Console.WriteLine("\nМинимальный элемент массива = " + minInArray);
+        }
+    }
+}
